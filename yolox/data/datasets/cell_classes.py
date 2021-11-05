9a6131ce980376f5695e0a8a76484ddff5809202
LiamTTT <liam.liusibo@gmail.com>
2021-11-05 16:09:44 +0800

    add cell dataset definition.

---
+++ yolox/data/datasets/cell_classes.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+'''
+@Author: LiamTTT
+@Email: nirvanalautt@gmail.com
+@Project: YOLOX
+@File: cell_classes.py
+@Date: 2021/11/05
+@Time: 下午4:52
+@Desc: Cell class, only 'pos' now, background always be 0
+'''
+
+CERVICAL_CELL_CLASSES = (
+    'pos',
+)
